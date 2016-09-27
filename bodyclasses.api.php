fc3ad0854db8b7630ee7ececf30cecc74fb8ed78
Rob Loach <robloach@gmail.com>
2016-09-27 12:30:07 -0400

    Add API documentation

--- bodyclasses.api.php
+++ bodyclasses.api.php
@@ -6,8 +6,25 @@
  */
 
 /**
- * @todo Add documentation.
+ * Exposes different methods to obtain classes for the body tag.
+ *
+ * @return array An associative array, keyed by machine name, where the "name"
+ *   represents a human readable name for the method, and "function" is a
+ *   callable function to retrieve the related classes.
+ *
+ * @see bodyclasses_bodyclasses_info()
  */
 function hook_bodyclasses_info() {
-  // @todo Add code documentation.
+  // Tell the Body Classes module that we can add classes about the Node to
+  // the body tag.
+  if (module_exists('node')) {
+    $bodyclasses['node'] = array(
+      // The human readable name.
+      'name' => t('Node'),
+      // The function that will return an array of all related classes.
+      'function' => 'mymodule_bodyclasses_node',
+    );
+  }
+
+  return $bodyclasses;
 }
